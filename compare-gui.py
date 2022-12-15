a42af533b3bf4769bd73b8ab0c22c1eee03cd137
Dobatymo <dobatymo@users.noreply.github.com>
2022-12-15 22:23:27 +0800

    refactor AspectRatioPixmapLabel and MyScrollArea to PixmapViewer
    add basic image viewer gui

--- compare-gui.py
+++ compare-gui.py
@@ -18,14 +18,11 @@ import numpy as np
 import pandas as pd
 from genutility._files import to_dos_path
 from genutility.time import MeasureTime
-from PIL import Image
-from pillow_heif import register_heif_opener
 from PySide2 import QtCore, QtGui, QtWidgets
 
 from prioritize import functions
+from shared_gui import PixmapViewer, read_qt_pixmap
 from utils import SortValuesKwArgs, pd_sort_groups_by_first_row, pd_sort_within_group, to_datetime
-
-register_heif_opener()
 
 APP_NAME = "compare-gui"
 PIC_CACHE_SIZE = 4
@@ -50,48 +47,7 @@ def set_reference_unchecked(df: pd.DataFrame) -> None:
     df.iloc[idx, df.columns.get_loc("checked")] = False
 
 
-@lru_cache(maxsize=PIC_CACHE_SIZE)
-def read_qt_pixmap(path: str) -> QtGui.QPixmap:
-
-    """Uses `pillow` to read a QPixmap from `path`.
-    This supports more image formats than Qt directly.
-    """
-
-    """ doesn't work...
-    # import this one late to use the correct qt lib
-    from PIL.ImageQt import toqpixmap
-    with Image.open(path) as img:
-        return toqpixmap(img)
-    """
-
-    modemap = {
-        "L": QtGui.QImage.Format_Grayscale8,
-        "RGB": QtGui.QImage.Format_RGB888,
-        "RGBA": QtGui.QImage.Format_RGBA8888,
-        "RGBX": QtGui.QImage.Format_RGBX8888,
-        "RGBa": QtGui.QImage.Format_RGBA8888_Premultiplied,
-    }
-
-    channelmap = {
-        "L": 1,
-        "RGB": 3,
-        "RGBA": 4,
-        "RGBX": 4,
-    }
-
-    with Image.open(path) as img:
-        if img.mode not in modemap or (img.width * channelmap[img.mode]) % 4 != 0:
-            # Unsupported image mode or image scanlines not 32-bit aligned
-            img = img.convert("RGBA")
-
-        # Are there any copies created below?
-        # Because the img buffer will be freed at the end of the block.
-        # Maybe Qt's `cleanupFunction` should be used instead of a context manager.
-
-        qimg_format = modemap[img.mode]
-        b = QtCore.QByteArray(img.tobytes())
-        qimg = QtGui.QImage(b, img.size[0], img.size[1], qimg_format)
-        return QtGui.QPixmap.fromImage(qimg)
+read_qt_pixmap = lru_cache(maxsize=PIC_CACHE_SIZE)(read_qt_pixmap)
 
 
 def show_in_file_manager(path: str) -> None:
@@ -112,91 +68,6 @@ def open_using_default_app(path: str) -> None:
         subprocess.call(["open", path])
     else:  # Linux variants
         subprocess.call(["xdg-open", path])
-
-
-class AspectRatioPixmapLabel(QtWidgets.QLabel):
-    def __init__(
-        self,
-        fit_to_widget: bool = True,
-        fixed_size: Optional[QtCore.QSize] = None,
-        parent: Optional[QtWidgets.QWidget] = None,
-    ) -> None:
-
-        """If `fit_to_widget` is True, the label will be resized to match the parent widgets size.
-        If it's False, it will be resized to the images original size.
-        """
-
-        super().__init__(parent)
-        self.setMinimumSize(1, 1)
-        self.setScaledContents(False)  # we do the scaling ourselves
-        self.setAlignment(QtCore.Qt.AlignCenter)
-        self.pm: Optional[QtGui.QPixmap] = None
-
-        # set properties
-        self.fit_to_widget = fit_to_widget
-        self.fixed_size = fixed_size
-
-    def clear(self) -> None:
-        super().clear()
-        self.pm = None
-
-    def _scaled_pixmap(self, size: QtCore.QSize) -> QtGui.QPixmap:
-        assert self.pm is not None
-        return self.pm.scaled(size, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
-
-    def resize_pixmap(self, size: QtCore.QSize):
-        # don't overwrite `super().resize()` here by accident
-        super().setPixmap(self._scaled_pixmap(size))
-
-    def scale(self) -> None:
-        assert self.pm is not None
-        if self.fixed_size:
-            self.resize_pixmap(self.fixed_size)
-            self.adjustSize()
-        elif self.fit_to_widget:
-            self.resize_pixmap(self.size())
-        else:
-            self.resize_pixmap(self.pm.size())
-            self.adjustSize()
-
-    def setPixmap(self, pm: QtGui.QPixmap) -> None:
-        self.pm = pm
-        self.scale()
-
-    @property
-    def fit_to_widget(self) -> bool:
-        return self._fit_to_widget
-
-    @fit_to_widget.setter
-    def fit_to_widget(self, value: bool) -> None:
-        self._fit_to_widget = value
-        if self.pm is not None:
-            self.scale()
-
-    @property
-    def fixed_size(self) -> Optional[QtCore.QSize]:
-        return self._fixed_size
-
-    @fixed_size.setter
-    def fixed_size(self, value: Optional[QtCore.QSize]) -> None:
-        self._fixed_size = value
-        if self.pm is not None:
-            self.scale()
-
-    # qt virtual
-
-    def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
-
-        # The label doesn't get `resizeEvent`s when `QScrollArea.widgetResizable()` is False.
-        # `resizeEvent`s are however triggered by the labels `self.adjustSize()`,
-        # so when setting a new pixmap, a resize event could still be triggered
-        # even if `self.fit_to_widget` is False.
-
-        # print("resizeEvent", self.fit_to_widget, self.fixed_size)
-
-        if self.pm is not None and self.fit_to_widget and self.fixed_size is None:
-            self.scale()
-        super().resizeEvent(event)
 
 
 def slice_to_list(value: slice) -> list:
@@ -659,17 +530,6 @@ class PrioritizeWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.prioritize_widget)
 
 
-class MyScrollArea(QtWidgets.QScrollArea):
-
-    arrow_keys = [QtCore.Qt.Key_Left, QtCore.Qt.Key_Right, QtCore.Qt.Key_Up, QtCore.Qt.Key_Down]
-
-    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
-        if event.key() in self.arrow_keys and event.modifiers() == QtCore.Qt.NoModifier:
-            event.ignore()
-        else:
-            super().keyPressEvent(event)
-
-
 class PictureWidget(QtWidgets.QWidget):
 
     picture_checked = QtCore.Signal(int, str, bool)
@@ -691,15 +551,12 @@ class PictureWidget(QtWidgets.QWidget):
         self.button2 = QtWidgets.QPushButton("Make &reference", self)
         self.button2.setCheckable(True)
         self.button2.clicked[bool].connect(self.on_make_reference)
-        self.label = AspectRatioPixmapLabel()
 
-        self.scroll = MyScrollArea(self)
-        self.scroll.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-        self.scroll.setWidget(self.label)
+        self.viewer = PixmapViewer(self)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.addWidget(self.scroll)
+        self.layout.addWidget(self.viewer)
 
         self.buttons = QtWidgets.QHBoxLayout()
         self.buttons.setContentsMargins(0, 0, 0, 0)
@@ -707,12 +564,8 @@ class PictureWidget(QtWidgets.QWidget):
         self.buttons.addWidget(self.button2)
         self.layout.addLayout(self.buttons)
 
-        self.pic_path = None
-        self.pic_group = None
-
-        # set properties
-        self._fixed_size = None  # set first
-        self.fit_to_window = True
+        self.pic_path: Optional[str] = None
+        self.pic_group: Optional[int] = None
 
     def on_check(self, checked: bool) -> None:
         if self.pic_path is not None:
@@ -729,30 +582,19 @@ class PictureWidget(QtWidgets.QWidget):
 
     @property
     def fit_to_window(self) -> bool:
-        return self._fit_to_window
+        return self.viewer.fit_to_window
 
     @fit_to_window.setter
     def fit_to_window(self, value: bool) -> None:
-        self._fit_to_window = value
-        if self.fixed_size is None:
-            self.scroll.setWidgetResizable(value)
-            self.label.fit_to_widget = value
-        else:
-            self.label._fit_to_widget = value
+        self.viewer.fit_to_window = value
 
     @property
     def fixed_size(self) -> Optional[Tuple[int, int]]:
-        return self._fixed_size
+        return self.viewer.fixed_size
 
     @fixed_size.setter
     def fixed_size(self, value: Optional[Tuple[int, int]]) -> None:
-        self._fixed_size = value
-        if value is None:
-            self.scroll.setWidgetResizable(self.label.fit_to_widget)
-            self.label.fixed_size = None
-        else:
-            self.scroll.setWidgetResizable(False)
-            self.label.fixed_size = QtCore.QSize(*value)
+        self.viewer.fixed_size = value
 
     def check_picture(self, group: int, path: str, checked: bool) -> bool:
         if group == self.pic_group and path == self.pic_path:
@@ -776,14 +618,14 @@ class PictureWidget(QtWidgets.QWidget):
         self.pic_path = path
 
         try:
-            self.pixmap = read_qt_pixmap(path)
+            pixmap = read_qt_pixmap(path)
 
             # fixme: This can disable scale normalization if there's no width/high information.
             # There are not GUI indicators for this however.
             if self.fixed_size is not None:  # only update if it was set previously
                 self.fixed_size = self.parent().model.get_group_max_size(group)
 
-            self.label.setPixmap(self.pixmap)
+            self.viewer.setPixmap(pixmap)
             self.button1.setChecked(checked)
             self.button1.setEnabled(not reference)
             self.button2.setChecked(reference)
@@ -791,7 +633,7 @@ class PictureWidget(QtWidgets.QWidget):
             return True
         except (FileNotFoundError, ValueError) as e:
             self.load_file_failed.emit(f"{type(e).__name__}: {e}")
-            self.label.clear()
+            self.viewer.clear()
             self.button1.setChecked(checked)
             self.button1.setEnabled(False)
             self.button2.setChecked(reference)
