d982893ce419d3523f288b054cdd80771b1c6889
Dobatymo <dobatymo@users.noreply.github.com>
2023-02-22 16:50:02 +0800

    limit numpy builtin threadpool

--- benchmark_dups.py
+++ benchmark_dups.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Optional, Tuple
 
 import dask.array as da
@@ -7,7 +8,7 @@ from genutility.iter import progress
 from genutility.time import PrintStatementTime
 from genutility.typing import SizedIterable
 
-from npmp import ChunkedParallel, SharedNdarray
+import npmp
 
 
 def l2_dups_chunk(a: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -15,35 +16,57 @@ def l2_dups_chunk(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     return np.argwhere(m < 1)
 
 
-def l2_dups_numpy(arr: np.ndarray):
+def matmul_chunk(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    shape = np.broadcast_shapes(a.shape, b.shape)
+    ab = np.broadcast_to(a, shape)
+    bb = np.broadcast_to(b, shape)
+    np.matmul(ab, bb)  # uses multi-threaded blas libary
+    return a
+
+
+funcs = {
+    "l2_dups_chunk": l2_dups_chunk,
+    "matmul_chunk": matmul_chunk,
+}
+
+
+def task_numpy(task: str, arr: np.ndarray):
+    assert task == "l2_dups_chunk"
     a_arr = arr[None, :, :]
     b_arr = arr[:, None, :]
     return l2_dups_chunk(a_arr, b_arr)
 
 
-def l2_dups_npmt(arr: np.ndarray, chunksize: int) -> SizedIterable[np.ndarray]:
+def task_npmt(task: str, arr: np.ndarray, chunksize: int, limit: Optional[int]) -> SizedIterable[np.ndarray]:
     if len(arr.shape) != 2:
         raise ValueError("Input must be a list of packed hashes (2-dimensional byte array)")
+
+    npmp.THREADPOOL_LIMIT = limit
 
     a_arr = arr[None, :, :]
     b_arr = arr[:, None, :]
 
-    return ChunkedParallel(l2_dups_chunk, a_arr, b_arr, (chunksize, chunksize), backend="threading", pass_coords=False)
+    return npmp.ChunkedParallel(
+        funcs[task], a_arr, b_arr, (chunksize, chunksize), backend="threading", pass_coords=False
+    )
 
 
-def l2_dups_npmp(sharr: SharedNdarray, chunksize: int) -> SizedIterable[np.ndarray]:
+def task_npmp(task: str, sharr: npmp.SharedNdarray, chunksize: int, limit: Optional[int]) -> SizedIterable[np.ndarray]:
     if len(sharr.shape) != 2:
         raise ValueError("Input must be a list of packed hashes (2-dimensional byte array)")
+
+    npmp.THREADPOOL_LIMIT = limit
 
     a_arr = sharr.reshape((1, sharr.shape[0], sharr.shape[1]))
     b_arr = sharr.reshape((sharr.shape[0], 1, sharr.shape[1]))
 
-    return ChunkedParallel(
-        l2_dups_chunk, a_arr, b_arr, (chunksize, chunksize), backend="multiprocessing", pass_coords=False
+    return npmp.ChunkedParallel(
+        funcs[task], a_arr, b_arr, (chunksize, chunksize), backend="multiprocessing", pass_coords=False
     )
 
 
-def l2_dups_dask(np_arr: np.ndarray, chunksize: int):
+def task_dask(task: str, np_arr: np.ndarray, chunksize: int):
+    assert task == "l2_dups_chunk"
     arr = da.from_array(np_arr, chunks=(chunksize, -1))
     a = da.broadcast_to(arr[None, :, :], (arr.shape[0], arr.shape[0], arr.shape[1]), chunks=(chunksize, chunksize, -1))
     b = da.broadcast_to(arr[:, None, :], (arr.shape[0], arr.shape[0], arr.shape[1]), chunks=(chunksize, chunksize, -1))
@@ -51,25 +74,30 @@ def l2_dups_dask(np_arr: np.ndarray, chunksize: int):
     return da.argwhere(m < 1).compute()
 
 
-def main(engine: str, dims: Tuple[int, int], chunksize: int, seed: Optional[int]) -> None:
+def main(
+    engine: str, task: str, dims: Tuple[int, int], chunksize: int, seed: Optional[int], limit: Optional[int]
+) -> None:
     rng = np.random.default_rng(seed)
     np_arr = rng.uniform(0, 1, size=dims).astype(np.float32)
 
     try:
         with PrintStatementTime():
             if engine == "numpy":
-                out = l2_dups_numpy(np_arr)
+                out = task_numpy(task, np_arr)
             elif engine == "npmt":
-                out = np.concatenate(list(progress(l2_dups_npmt(np_arr, chunksize))))
+                out = np.concatenate(list(progress(task_npmt(task, np_arr, chunksize, limit))))
             elif engine == "npmp":
-                out = np.concatenate(list(progress(l2_dups_npmp(SharedNdarray.from_array(np_arr), chunksize))))
+                out = np.concatenate(
+                    list(progress(task_npmp(task, npmp.SharedNdarray.from_array(np_arr), chunksize, limit)))
+                )
             elif engine == "dask":
                 with ProgressBar():
-                    out = l2_dups_dask(np_arr, chunksize)
+                    out = task_dask(task, np_arr, chunksize)
             else:
                 raise ValueError(engine)
     except MemoryError as e:
         print("MemoryError", e)
+        sys.exit(1)
     else:
         print(engine, out.shape)
 
@@ -83,7 +111,9 @@ if __name__ == "__main__":
     parser.add_argument("--engine", choices=("numpy", "npmt", "npmp", "dask"), required=True)
     parser.add_argument("--dims", type=int, nargs=2, required=True)
     parser.add_argument("--chunksize", type=int, default=CHUNKSIZE)
-    parser.add_argument("--seed", type=int)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--threadpool-limit", type=int, default=None)
+    parser.add_argument("--task", type=str, default="l2_dups_chunk")
     args = parser.parse_args()
 
-    main(args.engine, args.dims, args.chunksize, args.seed)
+    main(args.engine, args.task, args.dims, args.chunksize, args.seed, args.threadpool_limit)
