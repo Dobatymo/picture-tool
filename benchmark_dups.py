95eb48bbc31a83e334a89a0bb1a546fe854ae73d
Dobatymo <dobatymo@users.noreply.github.com>
2023-02-22 11:45:19 +0800

    add npmp threading backend
    add tests

--- benchmark_dups.py
+++ benchmark_dups.py
@@ -10,41 +10,62 @@ from genutility.typing import SizedIterable
 from npmp import ChunkedParallel, SharedNdarray
 
 
-def do(cls, arr):
-    a = arr[None, :, :]
-    b = arr[:, None, :]
-    m = cls.sqrt(cls.sum(cls.power(a - b, 2), axis=-1))
-    return cls.argwhere(m < 1)
-
-
-def l2_dups_chunk(a, b):
+def l2_dups_chunk(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     m = np.sqrt(np.sum(np.power(a - b, 2), axis=-1))
     return np.argwhere(m < 1)
 
 
-def l2_dups_npmp(sharr: SharedNdarray, chunksize: Tuple[int, int]) -> SizedIterable[np.ndarray]:
+def l2_dups_numpy(arr: np.ndarray):
+    a_arr = arr[None, :, :]
+    b_arr = arr[:, None, :]
+    return l2_dups_chunk(a_arr, b_arr)
+
+
+def l2_dups_npmt(arr: np.ndarray, chunksize: int) -> SizedIterable[np.ndarray]:
+    if len(arr.shape) != 2:
+        raise ValueError("Input must be a list of packed hashes (2-dimensional byte array)")
+
+    a_arr = arr[None, :, :]
+    b_arr = arr[:, None, :]
+
+    return ChunkedParallel(l2_dups_chunk, a_arr, b_arr, (chunksize, chunksize), backend="threading", pass_coords=False)
+
+
+def l2_dups_npmp(sharr: SharedNdarray, chunksize: int) -> SizedIterable[np.ndarray]:
     if len(sharr.shape) != 2:
         raise ValueError("Input must be a list of packed hashes (2-dimensional byte array)")
 
     a_arr = sharr.reshape((1, sharr.shape[0], sharr.shape[1]))
     b_arr = sharr.reshape((sharr.shape[0], 1, sharr.shape[1]))
 
-    return ChunkedParallel(l2_dups_chunk, a_arr, b_arr, chunksize, pass_coords=False)
+    return ChunkedParallel(
+        l2_dups_chunk, a_arr, b_arr, (chunksize, chunksize), backend="multiprocessing", pass_coords=False
+    )
 
 
-def main(engine: str, dims: Tuple[int, int], chunksize: Tuple[int, int], seed: Optional[int]) -> None:
+def l2_dups_dask(np_arr: np.ndarray, chunksize: int):
+    arr = da.from_array(np_arr, chunks=(chunksize, -1))
+    a = da.broadcast_to(arr[None, :, :], (arr.shape[0], arr.shape[0], arr.shape[1]), chunks=(chunksize, chunksize, -1))
+    b = da.broadcast_to(arr[:, None, :], (arr.shape[0], arr.shape[0], arr.shape[1]), chunks=(chunksize, chunksize, -1))
+    m = da.sqrt(da.sum(da.power(a - b, 2), axis=-1))
+    return da.argwhere(m < 1).compute()
+
+
+def main(engine: str, dims: Tuple[int, int], chunksize: int, seed: Optional[int]) -> None:
     rng = np.random.default_rng(seed)
-    np_arr = rng.uniform(0, 1, size=dims)
+    np_arr = rng.uniform(0, 1, size=dims).astype(np.float32)
 
     try:
         with PrintStatementTime():
             if engine == "numpy":
-                out = do(np, np_arr)
+                out = l2_dups_numpy(np_arr)
+            elif engine == "npmt":
+                out = np.concatenate(list(progress(l2_dups_npmt(np_arr, chunksize))))
             elif engine == "npmp":
                 out = np.concatenate(list(progress(l2_dups_npmp(SharedNdarray.from_array(np_arr), chunksize))))
             elif engine == "dask":
                 with ProgressBar():
-                    out = do(da, da.from_array(np_arr, chunks=chunksize)).compute()
+                    out = l2_dups_dask(np_arr, chunksize)
             else:
                 raise ValueError(engine)
     except MemoryError as e:
@@ -56,12 +77,12 @@ def main(engine: str, dims: Tuple[int, int], chunksize: Tuple[int, int], seed: O
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
-    CHUNKSIZE = (1000, 1000)
+    CHUNKSIZE = 1000
 
     parser = ArgumentParser()
-    parser.add_argument("--engine", choices=("numpy", "npmp", "dask"), required=True)
+    parser.add_argument("--engine", choices=("numpy", "npmt", "npmp", "dask"), required=True)
     parser.add_argument("--dims", type=int, nargs=2, required=True)
-    parser.add_argument("--chunksize", type=int, nargs=2, default=CHUNKSIZE)
+    parser.add_argument("--chunksize", type=int, default=CHUNKSIZE)
     parser.add_argument("--seed", type=int)
     args = parser.parse_args()
 
---
+++ test_npmp.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+import numpy as np
+from genutility.test import parametrize
+from genutility.typing import SizedIterable
+
+from npmp import ChunkedParallel, SharedNdarray
+
+
+def sum_chunk(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    return np.sum(a + b, axis=-1)
+
+
+class NpmpTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        seed = 0
+        rng = np.random.default_rng(seed)
+        cls.arr = rng.uniform(0, 1, size=(100, 100)).astype(np.float32)
+        cls.truth = sum_chunk(cls.arr, cls.arr)
+        cls.chunksize = 10
+
+    @parametrize(
+        (1,),
+        (10,),
+        (100,),
+        (1000,),
+    )
+    def test_mp(self, chunksize):
+        sharr = SharedNdarray.from_array(self.arr)
+        it = ChunkedParallel(
+            sum_chunk, sharr, sharr, (chunksize,), ordered=True, backend="multiprocessing", pass_coords=False
+        )
+        result = np.concatenate(list(it))
+        np.testing.assert_allclose(result, self.truth)
+
+    @parametrize(
+        (1,),
+        (10,),
+        (100,),
+        (1000,),
+    )
+    def test_mt(self, chunksize):
+        it = ChunkedParallel(
+            sum_chunk, self.arr, self.arr, (chunksize,), ordered=True, backend="threading", pass_coords=False
+        )
+        result = np.concatenate(list(it))
+        np.testing.assert_allclose(result, self.truth)
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
