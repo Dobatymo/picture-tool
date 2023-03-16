9745ea323367d04c4e7cf86e8b98477d1e7f7fad
Dobatymo <dobatymo@users.noreply.github.com>
2023-03-16 15:40:19 +0800

    add support for strided numpy arrays in npmp.SharedNdarray
    make annoy optional

--- picturetool/ml_utils.py
+++ picturetool/ml_utils.py
@@ -3,7 +3,6 @@ from typing import Iterable, Iterator, List, Tuple, Union, overload
 
 import faiss
 import numpy as np
-from annoy import AnnoyIndex
 from tqdm import tqdm
 
 from .utils import slice_idx
@@ -131,26 +130,3 @@ def faiss_to_pairs(it: Iterable[Tuple[int, int, float]]) -> Tuple[np.ndarray, np
         return np.array(pairs), np.array(dists)
     else:
         return np.empty(shape=(0, 2), dtype=np.int64), np.empty(shape=(0,), dtype=np.float32)
-
-
-def annoy_from_array(arr: np.ndarray, norm: str, n_trees: int = 100) -> AnnoyIndex:
-    if norm == "euclidean":
-        index = AnnoyIndex(arr.shape[1], norm)
-    elif norm == "hamming":
-        index = AnnoyIndex(arr.shape[1] * 8, norm)
-    else:
-        raise ValueError(f"Invalid norm: {norm}")
-
-    for i in range(arr.shape[0]):
-        index.add_item(i, arr[i])
-    index.build(n_trees)
-
-    return index
-
-
-def annoy_duplicates_top_k(index: AnnoyIndex, top_k: int, verbose: bool = False) -> np.ndarray:
-    out = []
-    for i in tqdm(range(index.get_n_items()), disable=not verbose):
-        items, distances = index.get_nns_by_item(i, top_k, include_distances=True)
-        out.append(items)
-    return np.array(out)
--- scripts/statistics.py
+++ scripts/statistics.py
@@ -1,7 +1,7 @@
 import logging
-from os import DirEntry, fspath
+import os
 from pathlib import Path
-from typing import Iterable, Union
+from typing import Iterable, Iterator, Union
 
 import pandas as pd
 from filemeta.exif import InvalidImageDataError, exif_table
@@ -9,7 +9,7 @@ from genutility.datetime import datetime_from_utc_timestamp_ns
 from genutility.pickle import cache
 from pandasql import sqldf
 
-EntryLike = Union[Path, DirEntry]
+EntryLike = Union[Path, os.DirEntry]
 
 logger = logging.getLogger(__name__)
 
@@ -99,7 +99,7 @@ ALL_COLUMNS = [
 def get_dataframe_by_iter(pathiter: Iterable[EntryLike]) -> pd.DataFrame:
     values = []
     for path in pathiter:
-        filepath = fspath(path)
+        filepath = os.fspath(path)
         filesize = path.stat().st_size
         mtime = path.stat().st_mtime_ns
 
@@ -111,31 +111,52 @@ def get_dataframe_by_iter(pathiter: Iterable[EntryLike]) -> pd.DataFrame:
         try:
             for ifd, key, key_label, value, value_label in exif_table(filepath):
                 values.append([filepath, ifd + "-" + key, ifd + " " + key_label, value, value_label])
-        except InvalidImageDataError:
-            logger.warning("%s is not a valid image file", path)
+        except InvalidImageDataError as e:
+            logger.warning("%s is not an image file with valid exif data: %s", path, e)
 
     df = pd.DataFrame(values, columns=("path", "key", "key_label", "value", "value_label"))
     df = df.pivot(index="path", columns="key_label", values="value_label")
     return df
 
 
-def get_dataframe_by_path(path):
-    def it(path):
+def get_dataframe_by_path(path: Path) -> pd.DataFrame:
+    extensions = {".jpg", ".jpeg", ".tif", ".tiff", ".webp"}  # ".wav", ".png" not supported by piexif
+
+    def it(path: Path) -> Iterator[Path]:
         for p in path.rglob("*"):
-            if p.suffix.lower() in {".jpg", ".tif", ".tiff", ".webp"}:  # ".wav", ".png" not supported by piexif
+            if p.suffix.lower() in extensions:
                 yield p
 
     return get_dataframe_by_iter(it(path))
 
 
 if __name__ == "__main__":
-    from argparse import ArgumentParser
+    from argparse import ArgumentParser, RawDescriptionHelpFormatter
     from datetime import timedelta
 
     from genutility.args import is_dir
 
-    parser = ArgumentParser()
-    parser.add_argument("path", nargs="+", type=is_dir, help="Input directory to scan")
+    epilog = r"""Examples
+
+- `%(prog)s "C:\Users\Public\Pictures" --groupby "0th Model"`
+
+Show a list of all different camera models found with a count of how often they are used.
+
+- `%(prog)s "C:\Users\Public\Pictures" --filter "0th Model" "iPhone XR" --select path`
+
+Find all pictures taken with an iPhone XR and show the file paths.
+
+- ``%(prog)s --sql "SELECT path, `0th Make`, `0th Model` FROM df WHERE `0th Model` LIKE '%%canon eos%%'" D:\``
+
+Find all pictures taken by a Canon EOS camera using SQL query.
+"""
+
+    parser = ArgumentParser(
+        description="Calculate exif statistics from picture files",
+        epilog=epilog,
+        formatter_class=RawDescriptionHelpFormatter,
+    )
+    parser.add_argument("paths", metavar="PATH", nargs="+", type=is_dir, help="Input directories to scan")
     parser.add_argument("--rebuild-cache", action="store_true", help="Forces a cache rebuild")
     parser.add_argument("--verbose", action="store_true", help="Debug output")
     parser.add_argument("--cache-path", metavar="PATH", type=Path, default=Path("cache"), help="Path to cache")
@@ -149,7 +170,12 @@ if __name__ == "__main__":
         choices=ALL_COLUMNS,
         help=f"Collect statistics for feature. Allowed features are: {', '.join(ALL_COLUMNS)}",
     )
-    group.add_argument("--filter", metavar=("FEATURE", "VALUE"), nargs=2)
+    group.add_argument(
+        "--filter",
+        metavar=("FEATURE", "VALUE"),
+        nargs=2,
+        help='Keep only files where FEATURE equals VALUE, ie. "0th Model" "iPhone XR"',
+    )
     group.add_argument("--sql", metavar="QUERY", help="Query by SQL. Use table `df`.")
     args = parser.parse_args()
 
@@ -163,15 +189,18 @@ if __name__ == "__main__":
         logging.basicConfig(level=logging.INFO)
 
     if args.rebuild_cache:
-        df = pd.concat(map(cache(args.cache_path, duration=timedelta(0))(get_dataframe_by_path), args.path))
+        df = pd.concat(map(cache(args.cache_path, duration=timedelta(0))(get_dataframe_by_path), args.paths))
     else:
-        df = pd.concat(map(cache(args.cache_path)(get_dataframe_by_path), args.path))
+        df = pd.concat(map(cache(args.cache_path)(get_dataframe_by_path), args.paths))
 
     df = df[~df.index.duplicated(keep="first")]
 
     if args.groupby:
         if args.select:
             parser.error("--select can only be used with --filter")
+
+        if args.groupby not in df.columns:
+            parser.error(f"--groupby value {args.groupby!r} not in data")
 
         result = df.fillna("N/A").groupby(args.groupby).size()
         if args.sort_values:
@@ -182,8 +211,8 @@ if __name__ == "__main__":
             parser.error("--sort-values can only be used with --groupby")
 
         key, value = args.filter
-        if key not in ALL_COLUMNS:
-            parser.error(f"--filter KEY={key} is not one of {ALL_COLUMNS}")
+        if key not in df.columns:
+            parser.error(f"--filter KEY={key!r} not in data")
 
         result = df[df[key] == value].reset_index()
         if args.select:
@@ -191,7 +220,11 @@ if __name__ == "__main__":
         result.dropna(axis="columns", how="all", inplace=True)
 
     elif args.sql:
-        result = sqldf(args.sql, locals())
+        """alternative:
+        import duckdb
+        result = duckdb.sql(args.sql).df()
+        """
+        result = sqldf(args.sql, {"df": df})
 
     if args.out:
         result.to_csv(args.out, index=False)
